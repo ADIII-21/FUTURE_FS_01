3eaf23996fdd9f5202925bdb70d63cdd8d754bd6
ADIII-21 <adigupta2121@gmail.com>
2025-07-10 18:25:57 +0530

    changes made in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import profilePic from '../planet111.jpg'
 const TYPING_WORDS = [
   'Aditya Gupta',
   'A Web Developer',
-  'A Future Software Engineer',
+  'A Software Engineer',
 ]
 
 export default function Hero() {
